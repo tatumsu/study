56845901c5c7e518703a987c9b3fc9d0c858b8a4
tatumsu <tatumsu@163.com>
2016-03-17 23:02:36 +0800

    refine

--- web/php/var.php
+++ web/php/var.php
@@ -1,20 +1,19 @@
-<<!DOCTYPE html>
+<!DOCTYPE html>
 <html>
 <head>
 	<title></title>
 </head>
 <body>
 <?php
-$x = 5;
-$y = 10;
-
 function myTest() {
-    global $x, $y;
-    $y = $x + $y;
+    static $x = 0;
+    echo $x;
+    $x++;
 }
 
 myTest();
-echo $y; // outputs 15
+myTest();
+myTest();
 ?>
 </body>
 </html>
